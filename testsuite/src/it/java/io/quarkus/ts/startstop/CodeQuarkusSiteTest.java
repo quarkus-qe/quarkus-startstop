b05658c4aa87d85f4ad3ebfe65afd67c8bcdbc72
Rostislav Svoboda <rsvoboda@redhat.com>
2022-06-23 10:36:17 +0200

    Use platform BOM

--- testsuite/src/it/java/io/quarkus/ts/startstop/CodeQuarkusSiteTest.java
+++ testsuite/src/it/java/io/quarkus/ts/startstop/CodeQuarkusSiteTest.java
@@ -135,8 +135,6 @@ public class CodeQuarkusSiteTest {
             return;
         }
         Path quarkusProductBomPath = Paths.get(System.getProperty("maven.repo.local")).resolve("com/redhat/quarkus/platform/quarkus-bom");
-        // https://issues.redhat.com/browse/QUARKUS-1743 ... using core BOM instead of platform one for now
-        quarkusProductBomPath = Paths.get(System.getProperty("maven.repo.local")).resolve("io/quarkus/quarkus-bom");
         try (Stream<Path> paths = Files.list(quarkusProductBomPath)) {
             List<String> folders = paths.filter(Files::isDirectory)
                     .map(path -> path.getFileName().toString())
