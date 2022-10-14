0310a8a23bfa5627108ec1f4e58ba3ca7752f928
Rostislav Svoboda <rsvoboda@redhat.com>
2022-10-14 14:34:31 +0200

    Append .exe for Windows binary

--- testsuite/src/it/java/io/quarkus/ts/startstop/utils/MvnCmds.java
+++ testsuite/src/it/java/io/quarkus/ts/startstop/utils/MvnCmds.java
@@ -21,7 +21,7 @@ public enum MvnCmds {
     NATIVE(new String[][]{
             Stream.concat(Stream.of("mvn", "clean", "compile", "package", "-Pnative"),
                     getQuarkusNativeProperties().stream()).toArray(String[]::new),
-            new String[]{Commands.isThisWindows ? "target\\quarkus-runner" : "./target/quarkus-runner"}
+            new String[]{Commands.isThisWindows ? "target\\quarkus-runner.exe" : "./target/quarkus-runner"}
     }),
     GENERATOR(new String[][]{
             new String[]{
