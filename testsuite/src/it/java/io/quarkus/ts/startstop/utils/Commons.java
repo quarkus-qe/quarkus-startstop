15c3138ba2e4dc9aede0dca6c1ee0b5ccbdb1eba
Fedor Dudinskiy <fdudinsk@redhat.com>
2023-01-31 15:37:30 +0100

    Refactoring
    Move several repeating patterns to another class

--- testsuite/src/it/java/io/quarkus/ts/startstop/utils/Commons.java
+++ testsuite/src/it/java/io/quarkus/ts/startstop/utils/Commons.java
@@ -3,6 +3,7 @@ package io.quarkus.ts.startstop.utils;
 import java.util.regex.Pattern;
 
 public class Commons {
-    static Pattern NETTY_HANDLERS=Pattern.compile(".*Could not register io.netty.handler.codec.*"); //https://github.com/quarkusio/quarkus/issues/30508
-    static Pattern NO_IMAGE = Pattern.compile(".*Tag 22.3-java17 was deleted or has expired.*"); //https://github.com/quarkusio/quarkus/issues/30738
+    static final Pattern CLOSED_STREAM = Pattern.compile(".*Stream is closed, ignoring and trying to continue.*"); // https://github.com/quarkusio/quarkus/pull/28810
+    static final Pattern STREAM_ERROR = Pattern.compile(".*java.lang.RuntimeException: Error reading stream.*"); // https://github.com/quarkusio/quarkus/issues/28799 (should be removed once 2.14.0.Final is out)
+    static Pattern NETTY_HANDLERS = Pattern.compile(".*Could not register io.netty.handler.codec.*"); //https://github.com/quarkusio/quarkus/issues/30508
 }
