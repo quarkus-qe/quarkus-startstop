48ba562dd3ae1e4083b68f85eb9f4688411e6d9c
Michal Jurc <mjurc@redhat.com>
2025-10-09 17:34:39 +0200

    QUARKUS-6714 - Allowlisting deps of langchain4j-parsers-base
    * Allowlisting unproductized runtime dependencies of
      langchain4j-parsers-base. For more information, see
      https://issues.redhat.com/browse/QUARKUS-6714.

--- testsuite/src/it/java/io/quarkus/ts/startstop/utils/WhitelistProductBomJars.java
+++ testsuite/src/it/java/io/quarkus/ts/startstop/utils/WhitelistProductBomJars.java
@@ -18,7 +18,19 @@ public enum WhitelistProductBomJars {
             "net.java.dev.jna.jna-platform",
             "com.github.victools.jsonschema-generator",  // https://issues.redhat.com/browse/QUARKUS-6713
             "org.apache.opennlp.opennlp-tools",          // https://issues.redhat.com/browse/QUARKUS-6715
-            "com.knuddels.jtokkit"                       // https://issues.redhat.com/browse/QUARKUS-6716
+            "com.knuddels.jtokkit",                      // https://issues.redhat.com/browse/QUARKUS-6716
+            // https://issues.redhat.com/browse/QUARKUS-6714
+            "org.apache.poi.poi",
+            "io.quarkiverse.poi.quarkus-poi",
+            "io.quarkus.quarkus-awt",
+            "org.apache.commons.commons-collections4",
+            "org.apache.commons.commons-math3",
+            "com.zaxxer.SparseBitSet",
+            "org.apache.poi.poi-ooxml",
+            "org.apache.xmlbeans.xmlbeans",
+            "com.github.virtuald.curvesapi",
+            "org.apache.poi.poi-ooxml-full",
+            "org.apache.poi.poi-scratchpad"
     });
 
     public final String[] jarNames;
