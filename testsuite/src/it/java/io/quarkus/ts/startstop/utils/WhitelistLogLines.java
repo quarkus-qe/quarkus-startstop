885509dad14050aff2ee757f642306b206b50827
Martin Ocenas <mocenas@redhat.com>
2026-07-28 09:45:13 +0200

    Whitelist docker failure

--- testsuite/src/it/java/io/quarkus/ts/startstop/utils/WhitelistLogLines.java
+++ testsuite/src/it/java/io/quarkus/ts/startstop/utils/WhitelistLogLines.java
@@ -49,6 +49,7 @@ public enum WhitelistLogLines {
             // TODO remove this when https://github.com/quarkusio/quarkus/issues/41016#issuecomment-3847062608 is fixed
             Pattern.compile(".*Option 'DynamicProxyConfigurationResources' is deprecated and might be removed in a future release.*"),
             // RHEL 8 uses docker cgroups v1 by default, those are considered deprecated in native build
+            Pattern.compile(".*Command docker \\(pid .*\\) completed but logged error.*"),
             Pattern.compile(".*Support for cgroup v1 is deprecated and planned to be removed.*"),
     }),
     FULL_MICROPROFILE(new Pattern[]{
